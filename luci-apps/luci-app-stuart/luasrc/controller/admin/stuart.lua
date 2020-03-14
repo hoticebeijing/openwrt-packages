259b8dded95f960d13a2b04c0c5e0ac65e052ae3
stuarthua <stuarthua.cn@gmail.com>
2020-03-15 04:11:54 +0800

    repost by shell.

--- luci-apps/luci-app-stuart/luasrc/controller/admin/stuart.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.admin.stuart",package.seeall)
-
-function index()
-	entry({"admin", "stuart"}, firstchild(), "Stuart", 89).dependent = false
-	local page
-	page = entry({"admin", "stuart", "helloworld"}, template("helloworld"), "HelloWorld", 1)
-	page.i18n = "base"
-    page.dependent = true
-end
