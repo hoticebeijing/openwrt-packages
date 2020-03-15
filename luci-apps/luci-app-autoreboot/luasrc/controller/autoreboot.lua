5d3e1101c794a3f04f3914b1655bcaf8245e3628
stuarthua <stuarthua.cn@gmail.com>
2020-03-16 04:43:26 +0800

    repost by shell.

--- luci-apps/luci-app-autoreboot/luasrc/controller/autoreboot.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.autoreboot",package.seeall)
-function index()
-    entry({"admin", "custom"}, firstchild(), "我的", 89).dependent = false
-    entry({"admin", "custom", "autoreboot"}, cbi("autoreboot"), _("Scheduled Reboot"), 700)
-end
